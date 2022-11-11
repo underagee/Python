fb5dd5a8609f88b87260eaa21a9c1c90cf429402
underagee <jymhatekah@st.ug.edu.gh>
2022-11-11 18:31:08 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,11 @@
+# Modify the code below, according to the comments
+
+# assign the value 13 to the variable books_read
+books_read = 13
+print(books_read)
+
+meals_eaten = 4
+meals_eaten = meals_eaten + 1
+
+# Fill in the print() to print the number of meals eaten
+print(meals_eaten)
