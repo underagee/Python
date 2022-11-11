beab3edead9d35cd064d62fcda7b586c70617f2c
JonathanHatekah <jymhatekah@st.ug.edu.gh>
2022-11-11 19:19:15 +0000

    creating a mad lips game

---
+++ madlips.py
@@ -0,0 +1,34 @@
+# Follow the instructions in the tab to the right
+# Write your mad libs program here
+name = input("What is your name? ")
+print("Welcome to Mad Lips game " + name)
+print("Enter some words to make up a recipe for preparing pizza.")
+
+
+#Asking the user for inputs
+first_adjective = input("An adjective: ")
+nationality = input("A nationality: ")
+person = input("Name of a person: ")
+first_noun = input("Enter the word 'dough': ")
+second_adjective = input("An adjective: ")
+second_noun = input("Enter the word 'crust': ")
+third_adjective = input("A vegetable: ")
+fourth_adjective = input("A type of cheese: ")
+fifth_adjective = input("The plural of a vegetable: ")
+third_noun = input("An appliance for baking: ")
+first_number = input("A number: ")
+shape = input("A shape: ")
+first_pizza = input("A type of pizza: ")
+second_pizza = input("Another type of pizza: ")
+second_number = input("A number: ")
+
+#The writing
+print(f"Pizza was invented by a/an {first_adjective} {nationality} chef named {person}.")
+print(f"To make pizza, you need to take a lump of {first_noun}, and make a thin, round {second_adjective} {second_noun}.")
+print(f"Then you cover it with {third_adjective} sauce, {fourth_adjective} and fresh chopped {fifth_adjective}")
+print(f"Next you have to bake it in a very hot {third_noun}")
+print(f"When it is done, cut it into {first_number} {shape}.")
+print(f"Some kids like {first_pizza} pizza the best, but my favorite is the {second_pizza} pizza.")
+print(f"If I could, I would eat pizza {second_number} times a day")
+
+
