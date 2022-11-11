5daac89689486fc9c473437eff88a3b0812efd0a
JonathanHatekah <jymhatekah@st.ug.edu.gh>
2022-11-11 19:04:40 +0000

    simple calculations

---
+++ calculations.py
@@ -0,0 +1,15 @@
+# Update the code below to add three numbers and print the total.
+# (1) Ask the user for a third input
+# (2) Store the user input as an integer in a variable called third_num
+# (3) Add the numbers together with +
+# (4) Display the total along with the message: "The sum of the three numbers is"
+
+first_num = int(input("enter first number: "))
+second_num = int(input("enter second number: "))
+# add code here to ask for a third number
+third_num = int(input("enter third number: "))
+#change the line below to add the third number
+total = first_num + second_num + third_num
+
+# edit the text that will show up
+print("The sum of the three numbers is: ", total)
