a058be715697e796d0f401eac69d9715dd9adb2e
JonathanHatekah <jymhatekah@st.ug.edu.gh>
2022-11-11 19:07:44 +0000

    f-string practice

---
+++ fstring.py
@@ -0,0 +1,13 @@
+first_num = float(input("Enter first number: "))
+second_num = float(input("Enter second number: "))
+result = first_num / second_num
+
+print(f"The result is {result:.3}")
+
+
+
+
+
+
+
+
