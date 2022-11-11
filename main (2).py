c202b9a15e4fb807290e1319a1a4eeee6ad5d973
JonathanHatekah <jymhatekah@st.ug.edu.gh>
2022-11-11 18:52:19 +0000

    second commit

---
+++ main (2).py
@@ -0,0 +1,16 @@
+# Follow the instructions in the comments
+
+a = 15
+b = 7
+
+# Print the sum (+) of a and b (this one is done for you)
+print(a + b)
+
+# Print the difference (-) of a and b
+print(a-b)
+# Print the product (*) of a and b
+print(a*b)
+# Print the quotient (/) of a and b
+print(a/b)
+# Print the remainder (%) of a and b
+print(a%b)
