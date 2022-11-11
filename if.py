8a018a01cbbe50070e87f9ab6bfd6bf1d4d2cd13
JonathanHatekah <jymhatekah@st.ug.edu.gh>
2022-11-11 19:23:45 +0000

    the if condition

---
+++ if.py
@@ -0,0 +1,6 @@
+# Use an if statement to print "Leave!" if the user responds with 'yes'
+is_awkward = input("Is the party awkward? ")
+# add your if statement here
+# ...
+if is_awkward == "yes":
+  print("Leave!")
