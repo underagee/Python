996bb478dff14842ea0aaf1fc10dca0e73355a33
JonathanHatekah <jymhatekah@st.ug.edu.gh>
2022-11-11 19:01:26 +0000

    inputting data

---
+++ input.py
@@ -0,0 +1,5 @@
+# Ask the user how many hours of sleep they get and store that as the variable your_sleep
+your_sleep = input("How many hours do you typically sleep each night")
+
+# print the statement: Doctors recommend 8 hours of sleep every night, and you sleep x hours most nights. X should be the input entered by the user
+print("Doctors recommend 8 hours of sleep every night and you sleep",your_sleep , "hours most nights.")
