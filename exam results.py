9c8ce30823f6c891a8fcc302f4034acc1a8d0102
JonathanHatekah <jymhatekah@st.ug.edu.gh>
2022-11-15 14:03:04 +0000

    asking for exams results and checking if they passed or failed

---
+++ exam results.py
@@ -0,0 +1,9 @@
+# Ask for the exam score
+score = float(input('Enter score: '))
+# Check to see if the score is > 70
+# Print "You passed!" if so
+# Print "Try again" otherwise
+if score > 70:
+  print("You passed!")
+else:
+  print("Try again")
