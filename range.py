438195b2168295b620cebdab848aa0ebee6b5032
JonathanHatekah <jymhatekah@st.ug.edu.gh>
2022-12-02 14:23:26 +0000

    The range function

---
+++ range.py
@@ -0,0 +1,14 @@
+# Practice writing for loops using range
+
+# print the numbers from 4 to 14
+for i in range(4,15):
+  print(i)
+# print the numbers from -10 to 10
+for i in range(-10,11):
+  print(i)
+# print the numbers from 1 to 20, counting by 2s
+for i in range(1,20,2):
+  print(i)
+# print the numbers from 1000 to 500, counting backwards by 100s
+for i in range(1000,500,-100):
+  print(i)
