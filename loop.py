5144d8919865719bf07f1d28d4ece2d86c935c54
JonathanHatekah <jymhatekah@st.ug.edu.gh>
2022-12-02 14:18:31 +0000

    printing numbers from 1 to 10 in a loop

---
+++ loop.py
@@ -0,0 +1,11 @@
+# Write a while loop that prints the integers 1 through 10
+
+# Create the iteration variable i
+i = 1
+
+# write your while loop here
+while (i <=10):
+  print(i)
+i +=1
+
+# Don't forget to change i inside the loop body
