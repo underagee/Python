40b0c0f78d3270b5e5ff7fb9e7c7ea4c8ea21d13
JonathanHatekah <jymhatekah@st.ug.edu.gh>
2022-12-02 14:42:18 +0000

    listing

---
+++ list.py
@@ -0,0 +1,12 @@
+# Practice accessing list items by their index.
+
+tallest_buildings = ["Burj Khalifa", "Merdeka 118", "Shanghai Tower", "Abraj Al-Bait Clock Tower", "Ping An International Finance Centre", "Lotte World Tower"]
+
+# Print out the first item in the list
+# Use the [] notation to access the item
+print(tallest_buildings[0])
+# Print out the third item in the list
+print(tallest_buildings[2])
+# Print a message by combining the second and last items in the list
+# ___ is taller than __
+print(f"{tallest_buildings[1]} is taller than {tallest_buildings[5]}")
