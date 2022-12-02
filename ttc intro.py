bc74b212392f9bc737d3ba593177f7ec370467a9
JonathanHatekah <jymhatekah@st.ug.edu.gh>
2022-12-02 17:52:37 +0000

    Introduction to the Tic Tac Toe game

---
+++ ttc intro.py
@@ -0,0 +1,14 @@
+blank_board = """
+  1   2   3
+1   |   |  
+ --- --- ---
+2   |   | 
+ --- --- ---
+3   |   |  
+"""
+
+name   = input("What is your name? ")
+print("Welcome to Tic Tac Toe, " + name + ". Here is our playing board:")
+print(blank_board)
+position = input("Enter a position (i.e., 1,1): ")
+print(position)
