172e7c79633e86c23b9942a9a209657043112f1b
JonathanHatekah <jymhatekah@st.ug.edu.gh>
2022-12-02 17:39:20 +0000

    The longest word in five inputs

---
+++ longest_word.py
@@ -0,0 +1,14 @@
+# Follow the instructions in the tab to the right
+# Write your code below
+words = []
+for i in range(5):
+  word = input("Enter a word: ")
+  words.append(word)
+longest_word =max(words, key=len)
+print(longest_word)
+  
+  
+
+  
+
+
