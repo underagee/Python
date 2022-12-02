43349c038707654bdf9cc499355fecff9b3a3101
JonathanHatekah <jymhatekah@st.ug.edu.gh>
2022-12-02 14:52:26 +0000

    printing the names of people in your squad

---
+++ squad.py
@@ -0,0 +1,7 @@
+# Write your solution below. See the Instructions tab.
+# Create a list of your squad members
+squad = ["Jonathan", "Joyful", "Precious", "Daniel", "Kimanzi"]
+# Use a loop to print each name
+print("Squad 8")
+for i in squad:
+  print(i)
