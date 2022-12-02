a92c70648b8e849b3319271954d5932cdf260bfa
JonathanHatekah <jymhatekah@st.ug.edu.gh>
2022-12-02 17:41:04 +0000

    Finding the average in a list

---
+++ Average.py
@@ -0,0 +1,7 @@
+# See the Instructions tab to the right
+# Find the average of the numbers in this list
+nums = [-34, -13, 28, -34, 17, 43, -11, -25, 16, -35, 129, 30, 120, 10, 40, -5, 51, 32, 134, 36, 81, 87, 26, 49, 67, 36, 137, 29, 108, 58, 30]
+sum_of_nums = sum(nums)
+len_of_nums = len(nums)
+average = sum_of_nums / len_of_nums
+print(f"The average of the numbers in the list is {average}")
