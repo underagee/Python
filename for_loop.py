7898012ca2f0f2f33d4ae9118b29ecd5f32be92c
JonathanHatekah <jymhatekah@st.ug.edu.gh>
2022-12-02 14:21:36 +0000

    for loop

---
+++ for_loop.py
@@ -0,0 +1,6 @@
+# Write a for loop which displays "Hello," plus each name in the list, e.g. "Hello Keno"
+
+list = ["William", "Keno", "Ope", "Mohammed", "Misan"]
+# add a for loop
+for i in list:
+  print(f"Hello {i}")
